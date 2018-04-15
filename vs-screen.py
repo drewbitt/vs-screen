f816a5cf1e39dfb4d06aaeb63cf7a33f348e72e0
drewbitt <andrew@drewbitt.com>
2018-04-15 18:33:29 +0100

    burned in pgs working. some documentation

--- vs-screen.py
+++ vs-screen.py
@@ -5,7 +5,6 @@ import subprocess
 import re
 import os
 import sys
-from collections import namedtuple
 
 core = vs.core
 
@@ -19,7 +18,6 @@ parser.add_argument('--subtitle-track', '-s', dest='sub_track', type=int, const=
                      help="Subtitle track for screenshots")
 
 args = parser.parse_args()
-
 filename = args.clip
 frames = args.frames
 sub_track = args.sub_track
@@ -27,6 +25,7 @@ num_frames = args.num_frames if args.num_frames is not None else 10
 
 
 def open_clip(path: str) -> vs.VideoNode:
+    '''Load clip into vapoursynth'''
     if path.endswith('ts'):  # .m2ts and .ts
         clip = core.lsmas.LWLibavSource(path)
     else:
@@ -36,6 +35,7 @@ def open_clip(path: str) -> vs.VideoNode:
 
 
 def get_frame_numbers(clip, n):
+    '''Get frame numbers to get screenshots of based off of length of clip/num screenshots'''
     length = len(open_clip(clip))
     frames = choices(range(length // 10, length // 10 * 9), k=n)
     frames = set([x // 100 for x in frames])
@@ -43,7 +43,9 @@ def get_frame_numbers(clip, n):
         frames.add(choice(range(length // 10, length // 10 * 9)) // 100)
     return [x * 100 for x in frames]
 
-def get_track_id(file, num):
+
+def get_sub_track_id(file, num):
+    '''Returns wanted sub track id and type of subs'''
     try:
         raw_info = subprocess.check_output(["mkvmerge", "-i", file],
                                             stderr=subprocess.STDOUT)
@@ -62,8 +64,10 @@ def get_track_id(file, num):
     else:
         return None, None
 
+
 def get_subs(file, save_path, track):
-    track_id, sub_type = get_track_id(file, track)
+    '''Extracts subs'''
+    track_id, sub_type = get_sub_track_id(file, track)
     if track_id == None:
         print("Error: Did not find subtitles")
         sys.exit(1)
@@ -93,6 +97,7 @@ def get_subs(file, save_path, track):
 
 
 def get_fonts(file, save_path):
+    '''Extracts fonts'''
     try:
         raw_info = subprocess.check_output(["mkvmerge", "-i", file],
                                             stderr=subprocess.STDOUT)
@@ -121,6 +126,7 @@ def get_fonts(file, save_path):
             print("Found font type without otf/ttf extension - still extracting")
             to_extract.append([attach[0], attach[2]])
         elif attach[1] != "application/x-truetype-font" and (ext == ".otf" or ext == ".ttf"):
+            # notify user about mismatch but still extract
             print("Found font with extension but not of correct type - still extracting")
             to_extract.append([attach[0], attach[2]])
 
@@ -130,7 +136,7 @@ def get_fonts(file, save_path):
 
     # I tried to combine this into one long string, list comprehension etc. but couldn't get it to work
     # going to call the command multiple times for each font for now :\
-    # think its an issue of the os.path.join to string?
+    # think its an issue of the os.path.join to string? even added quotes to the filename but nah
 
     try:
         for a in to_extract:
@@ -146,9 +152,65 @@ def get_fonts(file, save_path):
         print("ERROR: CalledProcessError: Could not extract fonts despite finding some")
         sys.exit(1)
 
-    sys.exit(0)
+
+def render_subs(clip, filename, subs_extension, folder_path, frames):
+    # what do i got
+    print(" ")
+    print(clip)
+    print(filename)
+    print(subs_extension)
+    print(folder_path)
+    print(frames)
+
+    # example has frames as times?
+    frame_times = []
+    for i in frames:
+        frame_times.append(int(i * 23.976 * 1000))
+
+    print(frame_times)
+
+    # first - deal with vobsubs and get subs filename
+    noext = os.path.splitext(os.path.basename(filename))[0]
+    if subs_extension == "VOBSUBS":
+        sub_files = [ os.path.join(folder_path, noext + ".sub"), os.path.join(folder_path, noext + ".idx") ]
+        # i'm not gonna implement this yet though
+    else:
+        sub_file = os.path.join(folder_path, noext + subs_extension)
+
+    # assumes some fps stuff at the moment
+    # b = core.std.BlankClip(width=clip.width, height=clip.height, format=vs.RGB24, length=len(clip),
+                          # fpsnum=24000, fpsden=1001)
+
+    if subs_extension == ".pgs" or subs_extension == "VOBSUB":
+        # vobsubs not actually implemented
+        rgb = core.sub.ImageFile(clip, file=sub_file, blend=True)
+        # rgb = core.sub.ImageFile(b, file=sub_file, blend=False)
+        # alpha = core.std.PropToClip(rgb)
+        return core.imwri.Write(rgb, "PNG", os.path.join(save_path, '%d.png'), compression_type="None")
+    '''else:
+        # gonna need to define charset here which idk atm
+        # [rgb, alpha] = core.sub.TextFile(b, file=sub_file, fontdir=folder_path, charset=charset, blend=False)
+        print("was not image")
+
+
+
+   if rgb.width != clip.width or rgb.height != clip.height:
+        rgb = core.resize.Spline36(rgb, width=clip.width, height=clip.height)
+        alpha = core.resize.Spline36(alpha, width=clip.width, height=clip.height)
+
+    alpha = core.std.Invert(alpha)
+
+    from functools import reduce
+    import operator
+    #Srgb = reduce(operator.add, map(lambda x: rgb[x], frame_times))
+    #Salpha = reduce(operator.add, map(lambda x: alpha[x], frame_times))
+
+    return core.imwri.Write(rgb, "PNG", os.path.join(save_path, '%d.png'), alpha=alpha, compression_type="None")
+    '''
+
 
 def parse_sub_type(type):
+    '''Gets file extension for subtitle type from mkvextract -i'''
     if type == "HDMV PGS":
         return ".pgs"
     elif type == "SubStationAlpha":
@@ -161,6 +223,7 @@ def parse_sub_type(type):
     else:
         print("Error: Didn't get a known sub type - exiting")
         sys.exit(1)
+
 
 if __name__ == '__main__':
 
@@ -181,10 +244,12 @@ if __name__ == '__main__':
     clip = open_clip(filename)
     save_path = os.path.join(os.getcwd(), dir_name)
     if sub_track:
+        # I don't gotta pass these global vars but I will darn it
         track_id, subs_extension = get_subs(filename, save_path, sub_track)
         get_fonts(filename, save_path)
-        sys.exit(0)
-    clip = imwri.Write(clip, 'png', os.path.join(save_path, '%d.png'))
+        clip = render_subs(clip, filename, subs_extension, save_path, frames)
+    else:
+        clip = imwri.Write(clip, 'png', os.path.join(save_path, '%d.png'))
 
     for frame in frames:
         print('Writing {:s}/{:d}.png'.format(save_path, frame))
